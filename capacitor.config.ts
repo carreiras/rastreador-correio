07b012082f57b215015c5088646fae52cd05b655
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-02-28 14:42:30 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'rastreador-correio',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
